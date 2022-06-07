f182335b18dce3008f7e7186566d5d3b22f4f749
翁俊梓 <1916222162@qq.com>
2022-06-07 23:55:25 +0800

    feat: antd-admin项目init

---
+++ docs/JSX/index.jsx
@@ -0,0 +1,15 @@
+/**
+ * @author wjz
+ * @date 2022/06/05 12:33
+ */
+//JSX
+const element1 = <h1>hello world!</h1> 
+//等价于 
+var element2 = React.createElement(
+  "h1",
+  null,
+  "hello world"
+);
+
+
+
